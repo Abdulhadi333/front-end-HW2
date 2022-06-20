9517eb3b1200868da32cbbd8a577079944a53a96
Abdulhadi <a-ahmasani@hotmail.com>
2022-06-20 23:35:52 +0300

    first commit

---
+++ img/ggg_files/config.js
@@ -0,0 +1,6 @@
+
+window[window["TiktokAnalyticsObject"]].instance("C32UU8VG09F1I5UEVEDG").setAdvancedMatchingAvailableProperties({"email":true,"phone_number":true,"auto_email":false,"auto_phone_number":false});
+window[window["TiktokAnalyticsObject"]].instance("C32UU8VG09F1I5UEVEDG").setPixelInfo && window[window["TiktokAnalyticsObject"]].instance("C32UU8VG09F1I5UEVEDG").setPixelInfo({status: 0, name: "Developer Mode ", advertiserID: "6730525654510469126", setupMode: 1, partner: "", is_onsite: false });
+window[window["TiktokAnalyticsObject"]].setPCMDomain && window[window["TiktokAnalyticsObject"]].setPCMDomain("jarir.com");
+window[window["TiktokAnalyticsObject"]].setPCMConfig && window[window["TiktokAnalyticsObject"]].setPCMConfig([]);
+window[window["TiktokAnalyticsObject"]].enableFirstPartyCookie && window[window["TiktokAnalyticsObject"]].enableFirstPartyCookie(true);
